aa5f33cb338fe68294ee7317567fb26b72d74a5e
Pedro Sudré <pedrosudre04@gmail.com>
2022-11-25 22:54:11 -0300

    fix

--- products/models.py
+++ products/models.py
@@ -6,7 +6,6 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
-#Custom queryset
 class ProductQuerySet(models.query.QuerySet):
     
     def active(self):
