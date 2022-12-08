b6632ecfb7df0301af8f462d44004175d3799478
Pedro Sudré <pedrosudre04@gmail.com>
2022-12-08 09:14:04 -0300

    update

--- tags/shell_comands.py
+++ tags/shell_comands.py
@@ -1,13 +1,5 @@
 #python manage.py shell
 
-from tags.models import Tag
-Tag.objects.all()
-azul = Tag.objects.last()
-azul.title
-azul.slug
-azul.products
-azul.products.all()
-
 from products.models import Product
 qs = Product.objects.all()
 qs
@@ -17,8 +9,6 @@ mouse.description
 mouse.tag
 mouse.tags
 
-#mas podemos usar tag_set
-#que é um gerenciador many to many
 mouse.tag_set.all()
 
 mouse.tag_set.filter(title__iexact='rgb')
